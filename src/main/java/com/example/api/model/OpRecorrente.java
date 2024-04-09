417683995a6f81461f1880e2333596df86438c17
ghmarconato <ghmarconato@gmail.com>
2024-04-09 19:47:41 -0300

    Add nossas classes

---
+++ src/main/java/com/example/api/model/OpRecorrente.java
@@ -0,0 +1,34 @@
+
+package com.example.api.model;
+
+import java.time.LocalDateTime;
+
+public class OpRecorrente extends Operacao {
+    private float valorRec;
+    private LocalDateTime dataInicial;
+    private long intervalo; // Em dias
+
+    public float getValorRec(){
+        return valorRec;
+    }
+
+    public LocalDateTime getData(){
+        return dataInicial;
+    }
+    public long getIntervalo(){
+        return intervalo;
+    }
+
+    public void setValor(float valorRec){
+        this.valorRec = valorRec;
+    }
+
+    public void setData(LocalDateTime dataInicial){
+        this.dataInicial = dataInicial;
+    }
+
+    public void setIntervalo(long intervalo){
+        this.intervalo = intervalo;
+    }
+
+}
--- src/main/java/com/example/api/model/Usuario.java
+++ src/main/java/com/example/api/model/Usuario.java
@@ -1,199 +1,96 @@
 package com.example.api.model;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import jakarta.persistence.CascadeType;
-import jakarta.persistence.ConstraintMode;
-import jakarta.persistence.DiscriminatorColumn;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.ForeignKey;
-import jakarta.persistence.UniqueConstraint;
-import com.example.api.model.Camera;
 
-//@MappedSuperclass
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "Usuario_Type")
-public class Usuario implements UserDetails {
+public class Usuario implements Serializable {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
 
-  private String login;
+  private String email;
   private String senha;
-  private UserRole role;
+  private String nome;
+  private LocalDateTime nascimento;
+  private String CPF;
+  private float valorConta;
 
-  // @JsonIgnore
-  // @org.hibernate.annotations.ForeignKey(name = "camera_id")
-  @JsonIgnore
-  @ManyToMany(fetch = FetchType.EAGER)
-  @JoinTable(name = "cameras_usuarios", uniqueConstraints = @UniqueConstraint(columnNames = { "camera_id",
-      "usuario_id" }, name = "unique_user_camera"), joinColumns = @JoinColumn(name = "usuario_id", referencedColumnName = "id", table = "usuario", unique = false), inverseJoinColumns = @JoinColumn(name = "camera_id", referencedColumnName = "id", table = "camera", unique = false))
-  private List<Camera> cameras = new ArrayList<Camera>();
 
-  @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
-  private List<Telefone> telefones = new ArrayList<Telefone>();
+  @OneToMany(mappedBy = "usuario", orphanRemoval = true, cascade = CascadeType.ALL)
+  private List<Operacao> operacoes = new ArrayList<Operacao>();
 
-  public Usuario() {
-
+  public long getId() {
+    return id;
   }
 
-  public Usuario(String login, String senha, String nome, UserRole role) {
-    this.login = login;
-    this.senha = senha;
-    this.role = role;
+  public String getEmail() {
+    return email;
   }
 
-  public Usuario(String login, String senha, UserRole role) {
-    this.login = login;
-    this.senha = senha;
-    this.role = role;
-  }
-
-  public String getLogin() {
-    return login;
-  }
-
-  public void setLogin(String login) {
-    this.login = login;
+  public String getNome() {
+    return nome;
   }
 
   public String getSenha() {
     return senha;
   }
 
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
   public void setSenha(String senha) {
     this.senha = senha;
   }
 
-  @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((id == null) ? 0 : id.hashCode());
-    return result;
+  public List<Operacao> getOperacoes() {
+    return operacoes;
   }
 
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj)
-      return true;
-    if (obj == null)
-      return false;
-    if (getClass() != obj.getClass())
-      return false;
-    Usuario other = (Usuario) obj;
-    if (id == null) {
-      if (other.id != null)
-        return false;
-    } else if (!id.equals(other.id))
-      return false;
-    return true;
+  public void setOperacoes(List<Operacao> operacoes) {
+    this.operacoes = operacoes;
   }
 
-  public Long getId() {
-    return id;
+  public String getCPF() {
+      return CPF;
   }
 
-  public void setId(Long id) {
-    this.id = id;
+  public void setCPF(String cPF) {
+      CPF = cPF;
   }
 
-  public List<Telefone> getTelefones() {
-    return telefones;
+  public LocalDateTime getNascimento() {
+      return nascimento;
   }
 
-  public void setTelefones(List<Telefone> telefones) {
-    this.telefones = telefones;
+  public void setNascimento(LocalDateTime nascimento) {
+      this.nascimento = nascimento;
   }
 
-  public List<Camera> getCameras() {
-    return cameras;
+  public float getValorConta() {
+      return valorConta;
   }
 
-  public void setCameras(List<Camera> cameras) {
-    this.cameras = cameras;
+  public void setValorConta(float valorConta) {
+      this.valorConta = valorConta;
   }
-
-  @Override
-  public Collection<? extends GrantedAuthority> getAuthorities() {
-    // O Spring Security já tem algumas roles implementadas. Repare que
-    // nesse método o retorno é uma colection, então cada usuário pode ter
-    // vários papéis (roles). Por exemplo, um ADMIN é ao mesmo tempo USER
-    // norma. Um CHEFE é ao mesmo tempo ADMIN e USER normal, ...
-    if (this.role == UserRole.ADMIN) {
-      return List.of(
-          new SimpleGrantedAuthority("ROLE_ADMIN"), // Admin
-          new SimpleGrantedAuthority("ROLE_USER") // é ao mesmo tempo user normal
-      );
-    } else {
-      return List.of(new SimpleGrantedAuthority("ROLE_USER"));
-    }
-  }
-
-  @Override
-  public String getPassword() {
-    // TODO Auto-generated method stub
-    return this.getSenha();
-  }
-
-  @Override
-  public String getUsername() {
-    // TODO Auto-generated method stub
-    return this.getLogin();
-  }
-
-  @Override
-  public boolean isAccountNonExpired() {
-    // TODO Auto-generated method stub
-    return true;
-  }
-
-  @Override
-  public boolean isAccountNonLocked() {
-    // TODO Auto-generated method stub
-    return true;
-  }
-
-  @Override
-  public boolean isCredentialsNonExpired() {
-    // TODO Auto-generated method stub
-    return true;
-  }
-
-  @Override
-  public boolean isEnabled() {
-    // TODO Auto-generated method stub
-    return true;
-  }
-
-  public UserRole getRole() {
-    return role;
-  }
-
-  public void setRole(UserRole role) {
-    this.role = role;
-  }
-
+  
 }
