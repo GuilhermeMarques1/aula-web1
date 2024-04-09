3f8bd55bb761dd05b4565142dd3efa4b0b3f5123
ghmarconato <ghmarconato@gmail.com>
2024-04-09 20:00:23 -0300

    delete useless code

--- src/main/java/com/example/api/repository/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.api.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.api.model.Produto;
-
-public interface ProdutoRepository extends CrudRepository<Produto, Long>{
-
-}
