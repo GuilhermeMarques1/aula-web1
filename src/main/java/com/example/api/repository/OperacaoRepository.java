367efdceddd5c503c34c9cab61eddba2d4ae0e1a
ghmarconato <ghmarconato@gmail.com>
2024-04-10 13:53:08 -0300

    Operacao FindByDate

--- src/main/java/com/example/api/repository/OperacaoRepository.java
+++ src/main/java/com/example/api/repository/OperacaoRepository.java
@@ -9,4 +9,8 @@ import com.example.api.model.Operacao;
 public interface OperacaoRepository extends CrudRepository<Operacao, Long> {
     @Query("select o from Operacao o where o.id = ?1")
 	Operacao findById(Long id);
+
+
+    @Query("select o from Operacao o join OpUnica ouni join OpRecorrente orc where ouni.data = ?1 OR orc.data = ?1")
+    Operacao findByDate();
 }
--- src/main/java/com/example/api/service/OperacaoService.java
+++ src/main/java/com/example/api/service/OperacaoService.java
@@ -1,5 +1,7 @@
 package com.example.api.service;
 
+import java.time.LocalDateTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
@@ -14,5 +16,10 @@ public class OperacaoService {
     @Override
     public UserDetails loadOperacaoById(Long id) throws UsernameNotFoundException {
         return operacaoRepository.findById(id);
+    }
+    
+    @Override
+    public UserDetails loadOperacaoById(LocalDateTime date) throws UsernameNotFoundException {
+        return operacaoRepository.findByDate(date);
     }   
 }
