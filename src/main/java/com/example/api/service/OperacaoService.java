e106752bb312895fe1283c8050338aab100d157a
ghmarconato <ghmarconato@gmail.com>
2024-04-09 22:09:45 -0300

    Create Operacao Service

---
+++ src/main/java/com/example/api/service/OperacaoService.java
@@ -0,0 +1,12 @@
+package com.example.api.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.api.repository.OperacaoRepository;
+
+@Service
+public class OperacaoService {
+    @Autowired
+    private OperacaoRepository operacaoRepository;
+}
