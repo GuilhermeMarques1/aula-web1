47e57d60ffe9c4ec95c53b9d4648010ab3b8be88
ghmarconato <ghmarconato@gmail.com>
2024-04-09 22:09:03 -0300

    create operacao controller

---
+++ src/main/java/com/example/api/controller/OperacaoController.java
@@ -0,0 +1,13 @@
+package com.example.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller("OperacaoController")
+@RequestMapping(value = "/operacao")
+
+public class OperacaoController {
+   
+}
