7a82159dbdc966ddbc803aa9cb29ee5fd4a0a917
ghmarconato <ghmarconato@gmail.com>
2024-04-13 16:42:07 -0300

    implements OpUnicaController
    Co-authored-by: Amanda Oliveira <AmandaOliveira3@users.noreply.github.com>
    Co-authored-by: Bianca Santos Pastos <biancapastos@users.noreply.github.com>

---
+++ src/main/java/com/example/api/controller/OpUnicaController.java
@@ -0,0 +1,40 @@
+package com.example.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
+
+import com.example.api.model.OpUnica;
+import com.example.api.repository.OpUnicaRepository;
+
+@Controller("OpUnicaController")
+@RequestMapping(value = "/operacao")
+
+public class OpUnicaController {
+
+    @Autowired
+    private OpUnicaRepository opUnicaRepository;
+
+    @GetMapping(value = "/", produces = "application/json")
+    public ResponseEntity<List<OpUnica>> getAllOperacoes() {
+        List<OpUnica> operacoes = (List<OpUnica>) this.opUnicaRepository.findAll();
+
+        return new ResponseEntity(operacoes, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/", produces = "application/json")
+    public ResponseEntity<OperacaoUnica> insertNewProduct(@RequestBody OperacaoUnica operacaoUnica) {
+
+        OperacaoUnica produtoSalvo = opUnicaRepository.save(operacaoUnica);
+
+        return new ResponseEntity(produtoSalvo, HttpStatus.OK);
+    }
+}
