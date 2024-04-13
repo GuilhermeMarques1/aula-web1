3f43cb3ad5ab9ea6b76bad5ff2922774cdfe7cc3
ghmarconato <ghmarconato@gmail.com>
2024-04-13 16:37:11 -0300

    implement CategoriaController
    Co-authored-by: Amanda Oliveira <AmandaOliveira3@users.noreply.github.com>
    Co-authored-by: Bianca Santos Pastos <biancapastos@users.noreply.github.com>

---
+++ src/main/java/com/example/api/controller/CategoriaController.java
@@ -0,0 +1,40 @@
+package com.example.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
+
+import com.example.api.repository.CategoriaRepository;
+import com.example.api.model.Categoria;
+
+
+@Controller("CategoriaController")
+@RequestMapping(value = "/categoria")
+
+public class CategoriaController {
+    @Autowired
+   private CategoriaRepository categoriaRepository;
+
+   @GetMapping(value="/", produces="application/json")
+   public ResponseEntity<List<Categoria>> getAllOperacoes(){
+    List<Categoria> operacoes = (List<Categoria>) this.categoriaRepository.findAll();
+
+    return new ResponseEntity(operacoes, HttpStatus.OK);
+   }
+
+   @PostMapping(value="/", produces="application/json")
+    public ResponseEntity<Categoria> insertNewProduct (@RequestBody Categoria categoria){
+
+        Categoria categoriaSalva = categoriaRepository.save(categoria);
+
+        return new ResponseEntity(categoriaSalva, HttpStatus.OK);
+    }    
+}
