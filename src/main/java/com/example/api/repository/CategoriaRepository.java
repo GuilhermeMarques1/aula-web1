0e2eda02ea6b7f5df9a236e7276896fa97bb54a7
ghmarconato <ghmarconato@gmail.com>
2024-04-14 15:59:30 -0300

    implement CategoriaRepository
    Co-authored-by: Amanda Oliveira <AmandaOliveira3@users.noreply.github.com>
    Co-authored-by: Bianca Santos Pastos <biancapastos@users.noreply.github.com>

--- src/main/java/com/example/api/repository/CategoriaRepository.java
+++ src/main/java/com/example/api/repository/CategoriaRepository.java
@@ -1,9 +1,13 @@
 package com.example.api.repository;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.api.model.Categoria;
 
 public interface CategoriaRepository extends CrudRepository<Categoria, Long> {
-
+    @Query("select c from Categoria c where c.id = ?1")
+	Optional<Categoria> findById(Long id);
 }
