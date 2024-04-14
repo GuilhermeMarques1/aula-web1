5cc02c3ff44748309d275288a908e25950f60df1
ghmarconato <ghmarconato@gmail.com>
2024-04-14 16:55:47 -0300

    ManyToMany in Operacao
    Co-authored-by: Amanda Oliveira <AmandaOliveira3@users.noreply.github.com>
    Co-authored-by: Bianca Santos Pastos <biancapastos@users.noreply.github.com>

--- src/main/java/com/example/api/model/Categoria.java
+++ src/main/java/com/example/api/model/Categoria.java
@@ -1,9 +1,18 @@
 package com.example.api.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.UniqueConstraint;
+
 
 @Entity
 public class Categoria {
@@ -12,6 +21,26 @@ public class Categoria {
     private Long id;
     private String Nome;
     private String Descricao;
+
+    @ManyToMany(fetch=FetchType.EAGER)
+    @JoinTable(name = "operacoes_categorias",
+      uniqueConstraints = @UniqueConstraint (
+      columnNames = {"operacao_id","categoria_id"}, 
+      name = "unique_operacao_categoria"
+    ), 
+    joinColumns = @JoinColumn(name = "categoria_id", 
+        referencedColumnName = "id", 
+        table = "categoria", 
+        unique = false
+    ), 
+    inverseJoinColumns = @JoinColumn (
+        name = "usuario_id", 
+        referencedColumnName = "id", 
+        table = "usuario", 
+        unique = false
+    )
+    )    
+  private List<Usuario> categorias = new ArrayList<Usuario>();
 
     public String getDescricao() {
         return Descricao;
